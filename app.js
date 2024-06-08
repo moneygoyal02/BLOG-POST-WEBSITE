b078696d6fe26077b005a183a8da373e77d25968
moneygoyal02 <goyalmoney95@gmail.com>
2024-06-08 13:58:54 +0530

    setup express

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get('', (req,res) => {
+    res.send("HELLO WORLD");
+});
+
+app.listen(PORT, () => {
+    console.log(`App Listening on Port ${PORT}`);
+});
