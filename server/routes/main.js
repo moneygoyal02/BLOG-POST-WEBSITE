f78a302667d3d5e24772f61d2c29a31174bd8d1b
moneygoyal02 <goyalmoney95@gmail.com>
2024-06-08 19:11:51 +0530

    create route and different pages like about and main page but we use use same html for all that

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('',(req,res) => {
-    res.send("hello world");
+router.get('/',(req,res) => {
+    res.render('index');
 });
+
+router.get('/about',(req,res) => {
+    res.render('about');
+});
+
 
 module.exports = router ; 
