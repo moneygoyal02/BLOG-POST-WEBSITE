b59a3fe877f4bec68f55a2ec4e0b1759a63c458d
moneygoyal02 <goyalmoney95@gmail.com>
2024-06-09 16:12:57 +0530

    let's do the route which will display the blog post so in order to do this let's go back all we need to know is that this link here goes to post and then we're passing the ID of the blog post and in this case you could swap it with a slug in future if you wish to develop that as well and now let's do that so under the main.js which is the old main route we're gonna create one more so I'm gonna copy this and maybe paste it around here this is going to be a get route and it's gonna be the post and we're passing the ID I'm actually going to copy this one here and paste it and then remove all the comments and in this case let's change a couple of things so first of all we this is going to be the get route and the page that we want to get is going to be called post and then we can put the ID in here so this is how we're gonna grab the ID and I'm gonna show you how we can do that now then inside here I'm Gonna Leave the logos as it is maybe we could change it to the actual title we'll have a look in a second and to grab the ID it's actually fairly simple what we can do let's move everything inside the try catch here first of all so like so and now to grab the ID what we can do is let let's prep a select equals request Dot param and then ID this is how we grab the ID sorry this is params and now we can query the database with this ID to do this we can modify this one here so post dot find by ID and then inside here in curly bracket we can put underscore ID is what we want to query and we want to create the slug which we're grabbing and the last thing that we want to do is create a page at the moment we're rendering the index page which is the home page so in this case I'm gonna create a new one code post so let's go back and inside here we can create a new page let's create a new one called post.ejs and for the post I'm going to do an H1 tag and inside here we can use ejs so ejs out and then we can put data dot title remove this one here and now I can do the article so let's say let's say article and inside here we can give it the class name of article maybe the H1 needs to be inside as well but then we can get the body text by doing ejs data dot body like so and that's it so if you go back to the website refresh make sure that everything was saved and if we click on any of the blog posts now we should be able to get the title and the blog players I didn't add too much text that's why there isn't much and this looks overly large compared to the paragraph here but that's something you can change with CSS and now if you go back and click on another one you will see that it's all working if we go back in order to potentially change the local title we could move it around here first of all so we could do this when we're getting the data we could potentially just do data dot title and we could do the sample well we don't really have a short description maybe you can incorporate

--- server/routes/main.js
+++ server/routes/main.js
@@ -47,6 +47,33 @@ router.get('/about',(req,res) => {
 });
 
 
+
+/**
+ * GET /
+ * Post :id
+*/
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await POST.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+
 // function insertPostData () {
 //   POST.insertMany([
 //     {
