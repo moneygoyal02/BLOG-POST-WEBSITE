62a7384757a927d85a316d6618bb86604cbde488
moneygoyal02 <goyalmoney95@gmail.com>
2024-06-12 16:15:55 +0530

    admin page

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -50,4 +50,51 @@ router.post('/register', async (req, res) => {
 });
 
 
+
+/**
+ * POST /
+ * Admin - Check Login
+*/
+router.post('/admin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    
+    const user = await User.findOne( { username } );
+
+    if(!user) {
+      return res.status(401).json( { message: 'Invalid credentials' } );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+      return res.status(401).json( { message: 'Invalid credentials' } );
+    }
+
+    const token = jwt.sign({ userId: user._id}, 'my blog' );
+    res.cookie('token', token, { httpOnly: true });
+    res.redirect('/dashboard');
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+/**
+ * GET /
+ * Admin Dashboard
+*/
+router.get('/dashboard', async (req, res) => {
+  
+
+    
+    res.render('admin/dashboard'
+     );
+
+ 
+
+});
+
+
 module.exports = router;
